ed6001089ee2ba5719caef1a6b9e0946c6604cf1
ryuzy_mac <onacool00700@gmail.com>
2021-09-14 13:56:12 +0900

    added func B-2

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Func B-1 Welcome to{' '}
+          Func B-2 Welcome to{' '}
           <a href="https://nextjs.org">Next.js + TypeScript!</a>
         </h1>
 
